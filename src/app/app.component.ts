720c327dda17753500cbb36ce141ac28bea136d7
Damal Ranula <ranuladamal@gmail.com>
2024-02-12 14:53:13 +0530

    add nav and view-all-customer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './common/nav/nav.component';
+import { ViewAllCustomerComponent } from './page/view-all-customer/view-all-customer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavComponent,ViewAllCustomerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
